f44e0c50d7bdb38c043a3ad8ed5a7f7f1a8994e1
alexangria1992 <alex040892@gmail.com>
2024-02-13 13:18:55 +1300

    Gramicci Section

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ let currentSlide = 0;
 const slides = document.querySelectorAll(".slide");
 const slideImages = document.querySelectorAll(".slide img");
 const dots = document.querySelectorAll(".dot");
+const primaryHeader = document.querySelector(".primary-header");
+const scrollWatcher = document.createElement("div");
+
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+  // console.log(entries);
+  primaryHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+});
+
+navObserver.observe(scrollWatcher);
 
 const size = slideImages[0].clientWidth;
 
